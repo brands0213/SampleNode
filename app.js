122badfe9487767546371affb63997030c97974f
brands0213 <90987267+brands0213@users.noreply.github.com>
2022-04-09 16:21:24 +0800

    third commit

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.get("/", function(req, res){
     res.send("Congratulations! Welcome to your First APP")
 })
 
+app.get("/new", function(req, res){
+    res.send("Welcome to your First APP")
+})
+
+
+
+
 
 
 app.listen(process.env.PORT || 3000)
